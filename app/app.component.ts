f30ec892840764f530c4f505c347703bccc58bf5
hatuey@gmail.com <Hatuey>
2017-01-16 02:35:04 -0600

    .

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,46 +1,11 @@
 import { Component } from '@angular/core';
 
-export class Hero {
-    id: number;
-    name: string;
-}
-
-@Component({
-  selector: 'my-app',
-  template:
-`
-      <h1>{{title}}</h1>
-      <h2>My Heroes</h2>
-      <ul class="heroes">
-        <li *ngFor="let hero of heroes">
-          <span class="badge">{{hero.id}}</span> {{hero.name}}
-        </li>
-      </ul>
-      <h2>{{hero.name}} destails!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input type="text" [(ngModel)]="hero.name" placeholder="name" />
-      </div>
-`,
+@Component ({
+    selector: "my-app",
+    template: `
+       <h1>{{title}}</h1>
+    `,
 })
-export class AppComponent  {
-    heroes: Hero[] = [
-        { id: 11, name: "Mr. Nice" },
-        { id: 12, name: "Narco" },
-        { id: 13, name: "Bombasto" },
-        { id: 14, name: "Celeritas" },
-        { id: 15, name: "Magneta" },
-        { id: 16, name: "RubberMan" },
-        { id: 17, name: "Dynama" },
-        { id: 18, name: "Dr IQ" },
-        { id: 19, name: "Magma" },
-        { id: 20, name: "Tornado" }
-    ];
-    // heroes = HEROES;
+export class AppComponent {
     title: string = "Tour of Heroes";
-    hero: Hero = {
-            id: 1,
-            name: "Windstorm"
-    };
 }
